aa4674aa06fc535418f078c4d6d329aad2992f5b
mdbappymia <mbm.21.02.16@gmail.com>
2021-12-20 21:18:37 +0600

    complete

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="bg-black">
       <Container className="footer-page-content">
         <h3 className=" py-md-5 pt-3">MD. BAPPY MIA</h3>
-        <p className="pb-sm-3 py-md-5 pt-3 text-secondary">
+        <p className="pb-sm-3 py-md-5 pt-3 m-0 text-secondary">
           2021 Copyright &copy; All right received
         </p>
       </Container>
