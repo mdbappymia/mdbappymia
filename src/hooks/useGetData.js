5b28de4fd760a98e9e4980761b3a29178885c330
mdbappymia <mbm.21.02.16@gmail.com>
2022-01-02 13:50:28 +0600

    async await use for handle get data from server

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -6,33 +6,47 @@ const useGetData = () => {
   const [services, setServices] = useState([]);
   const [reviews, setReviews] = useState([]);
   //   blogs data
+  // useEffect(() => {
+  //   fetch("https://mysterious-hollows-45831.herokuapp.com/blogs")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setBlogs(data.reverse());
+  //       fetch("https://mysterious-hollows-45831.herokuapp.com/projects")
+  //         .then((res) => res.json())
+  //         .then((data) => {
+  //           setProjects(data);
+  //           fetch("https://mysterious-hollows-45831.herokuapp.com/services")
+  //             .then((res) => res.json())
+  //             .then((data) => {
+  //               setServices(data);
+  //               fetch("https://mysterious-hollows-45831.herokuapp.com/reviews")
+  //                 .then((res) => res.json())
+  //                 .then((data) => setReviews(data));
+  //             });
+  //         });
+  //     });
+  // }, []);
+
+  const getAllDate = async () => {
+    const servicesDate = await fetch(
+      "https://mysterious-hollows-45831.herokuapp.com/services"
+    ).then((res) => res.json());
+    setServices(servicesDate);
+    const projectsData = await fetch(
+      "https://mysterious-hollows-45831.herokuapp.com/projects"
+    ).then((res) => res.json());
+    setProjects(projectsData);
+    const blogsData = await fetch(
+      "https://mysterious-hollows-45831.herokuapp.com/blogs"
+    ).then((res) => res.json());
+    setBlogs(blogsData.reverse());
+    const reviewsData = await fetch(
+      "https://mysterious-hollows-45831.herokuapp.com/reviews"
+    ).then((res) => res.json());
+    setReviews(reviewsData);
+  };
   useEffect(() => {
-    fetch("https://mysterious-hollows-45831.herokuapp.com/blogs")
-      .then((res) => res.json())
-      .then((data) => setBlogs(data.reverse()));
-  }, []);
-
-  //   projects data
-
-  useEffect(() => {
-    fetch("https://mysterious-hollows-45831.herokuapp.com/projects")
-      .then((res) => res.json())
-      .then((data) => setProjects(data));
-  }, []);
-
-  //   service data
-
-  useEffect(() => {
-    fetch("https://mysterious-hollows-45831.herokuapp.com/services")
-      .then((res) => res.json())
-      .then((data) => setServices(data));
-  }, []);
-  //   Reviews data
-
-  useEffect(() => {
-    fetch("https://mysterious-hollows-45831.herokuapp.com/reviews")
-      .then((res) => res.json())
-      .then((data) => setReviews(data));
+    getAllDate();
   }, []);
   return { blogs, projects, services, reviews };
 };
