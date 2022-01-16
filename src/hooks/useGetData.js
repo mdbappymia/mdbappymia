62000148918518627f5fdeeb739aaa3f69077960
mdbappymia <mbm.21.02.16@gmail.com>
2022-01-16 23:14:06 +0600

    gell all data one api

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -6,6 +6,7 @@ const useGetData = () => {
   const [services, setServices] = useState([]);
   const [reviews, setReviews] = useState([]);
   const [preloading, setPreloading] = useState(true);
+  const [recall, setRecall] = useState(false);
 
   useEffect(() => {
     fetch("https://mysterious-hollows-45831.herokuapp.com/allData")
@@ -16,8 +17,11 @@ const useGetData = () => {
         setBlogs(data.blogs);
         setReviews(data.reviews);
       })
+      .catch((e) => {
+        setRecall(!recall);
+      })
       .finally(() => setPreloading(false));
-  }, []);
+  }, [recall]);
 
   return { blogs, projects, services, reviews, preloading };
 };
